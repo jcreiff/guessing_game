b3ea110dcc183197cce7490e2e726de1c9e0dbcd
Joseph Reiff <jreiff@Josephs-MacBook-Air.local>
2015-05-05 14:35:59 -0400

    limited guesses to 5 with guess_count break

--- guessing_game.rb
+++ guessing_game.rb
@@ -8,12 +8,21 @@ def check_number(guess, secret_number)
   end
 end
 
+
 secret_number = rand(1..100)
-print "Guess a number between 1 and 100: "
+print "You have 5 chances to guess a number between 1 and 100: "
 guess = gets.chomp.to_i
+guess_count = 1
 
 while guess != secret_number
   check_number(guess, secret_number)
   guess = gets.chomp.to_i
+  guess_count += 1
+  break if guess_count == 5
 end
-puts "Correct! The number was #{secret_number}!"
+
+if guess == secret_number
+  puts "Correct! The number was #{secret_number}!"
+else
+  puts "Too late! The number was #{secret_number}!"
+end
