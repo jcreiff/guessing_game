10d8f7d4d6ee4a8a4d444eb559fa3f0011a73813
Joseph Reiff <jreiff@Josephs-MacBook-Air.local>
2015-05-05 13:58:00 -0400

    created check_number method

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,21 +1,19 @@
-check_number =
-
+def check_number(guess, secret_number)
+  if guess == secret_number
+    puts "Correct! The number was #{secret_number}!"
+  elsif guess > secret_number
+    print "Too high! Try again: "
+  else
+    print "Too low! Try again: "
+  end
+end
 
 secret_number = rand(1..100)
 print "Guess a number between 1 and 100: "
 guess = gets.chomp.to_i
 
-  if guess == secret_number
-    puts "Correct! The number was #{secret_number}!"
-  else
-    while guess != secret_number
-      if guess > secret_number
-        print "Too high! Try again: "
-        guess = gets.chomp.to_i
-      else
-        print "Too low! Try again: "
-        guess = gets.chomp.to_i
-      end
-    end
-    puts "Correct! The number was #{secret_number}!"
-  end
+while guess != secret_number
+  check_number(guess, secret_number)
+  guess = gets.chomp.to_i
+end
+puts "Correct! The number was #{secret_number}!"
