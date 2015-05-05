6cab3d1b938b249b21a24d54487dce8829d81f20
Joseph Reiff <jreiff@Josephs-MacBook-Air.local>
2015-05-05 13:48:37 -0400

    changed puts to print in while

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,3 +1,6 @@
+check_number =
+
+
 secret_number = rand(1..100)
 print "Guess a number between 1 and 100: "
 guess = gets.chomp.to_i
@@ -7,10 +10,10 @@ guess = gets.chomp.to_i
   else
     while guess != secret_number
       if guess > secret_number
-        puts "Too high! Try again: "
+        print "Too high! Try again: "
         guess = gets.chomp.to_i
       else
-        puts "Too low! Try again: "
+        print "Too low! Try again: "
         guess = gets.chomp.to_i
       end
     end
