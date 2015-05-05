caeeead0a5c8ea173e36a15039762d78afe86b50
Joseph Reiff <jreiff@Josephs-MacBook-Air.local>
2015-05-05 15:30:58 -0400

    added && to check_number, to fix nonsense output when user loses

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,22 +1,24 @@
-def check_number(guess, secret_number)
+def check_number(guess, secret_number, guess_count)
   if guess == secret_number
     puts "Correct! The number was #{secret_number}!"
-  elsif guess > secret_number
+  elsif guess > secret_number && guess_count != 5
     print "Too high! Try again: "
-  else
+  elsif guess < secret_number && guess_count != 5
     print "Too low! Try again: "
+  else
   end
 end
 
 def check_array(guess_array, guess)
   if guess_array.include?(guess)
-    print "Steve Perry would appreciate that you don't stop believing, but still: "
+    print "Steve Perry would appreciate that you don't stop believing, but that's still not correct. "
   else
     guess_array<<guess
   end
 end
 
 secret_number = rand(1..100)
+puts secret_number
 print "You have 5 chances to guess a number between 1 and 100: "
 guess = ""
 guess_count = 0
@@ -24,9 +26,9 @@ guess_array = []
 
 while guess != secret_number
   guess = gets.chomp.to_i
-  check_array(guess_array, guess)
-  check_number(guess, secret_number)
   guess_count += 1
+  check_array(guess_array, guess)
+  check_number(guess, secret_number, guess_count)
   break if guess_count == 5
 end
 
