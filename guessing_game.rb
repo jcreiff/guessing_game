c5769dd0385006ab33922866e02293677304ec6c
Joseph Reiff <jreiff@Josephs-MacBook-Air.local>
2015-05-05 15:06:27 -0400

    added check_array method for sarcasm. need to fix 'you lose' output

--- guessing_game.rb
+++ guessing_game.rb
@@ -8,21 +8,28 @@ def check_number(guess, secret_number)
   end
 end
 
+def check_array(guess_array, guess)
+  if guess_array.include?(guess)
+    print "Steve Perry would appreciate that you don't stop believing, but still: "
+  else
+    guess_array<<guess
+  end
+end
 
 secret_number = rand(1..100)
 print "You have 5 chances to guess a number between 1 and 100: "
-guess = gets.chomp.to_i
-guess_count = 1
+guess = ""
+guess_count = 0
+guess_array = []
 
 while guess != secret_number
-  check_number(guess, secret_number)
   guess = gets.chomp.to_i
+  check_array(guess_array, guess)
+  check_number(guess, secret_number)
   guess_count += 1
   break if guess_count == 5
 end
 
-if guess == secret_number
-  puts "Correct! The number was #{secret_number}!"
-else
+if guess != secret_number
   puts "Too late! The number was #{secret_number}!"
 end
